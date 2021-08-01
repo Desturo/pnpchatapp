b7192237da3d625019bf7b5ef06a6a88d438b1da
Desturo <lolomat25@gmai.com>
2021-08-02 00:27:20 +0200

    field for max value of dice only takes numbers now, few changes to labels

--- public/js/main.js
+++ public/js/main.js
@@ -35,6 +35,15 @@ chatForm.addEventListener('submit', (e) => {
     e.target.elements.msg.focus();
 });
 
+function onlyNumberKey(evt) {
+          
+    // Only ASCII character in that range allowed
+    var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+        return false;
+    return true;
+}
+
 function outputMessage(message) {
     const div = document.createElement('div');
     div.classList.add('message');
